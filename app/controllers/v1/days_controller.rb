206a204cad14575b0f1bd1dabb1df2aaad3d670d
AliceKB <alicekb01@gmail.com@gmail.com>
2017-04-17 14:53:15 -0400

    Created Days reset route
    Created a delete route for Days that deletes its meals but keeps the Day model.

--- app/controllers/v1/days_controller.rb
+++ app/controllers/v1/days_controller.rb
@@ -5,5 +5,11 @@ module V1
       day = Day.find(params[:id])
       render json: day
     end
+
+    def reset
+      day = Day.find(params[:id])
+      day.destroy_associations
+      render json: day
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   namespace :v1 do
     resources :days, only: [:show]
+    delete 'days/reset/:id', :to => 'days#reset'
     resources :weeks, only: [:index, :show, :create, :destroy]
     resources :meals, only: [:show, :create, :destroy]
   end
