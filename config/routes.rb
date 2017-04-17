8da3c160b1263a1cee4e2fa6d0dec102ab4dec83
AliceKB <alicekb01@gmail.com@gmail.com>
2017-04-17 16:07:19 -0400

    removed Home Controller
    Removed home controller and home route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'auth_user' => 'authentication#authenticate_user'
-  get 'home' => 'home#index'
 
   namespace :v1 do
     resources :days, only: [:show]
