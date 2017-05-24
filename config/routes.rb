3a74282d6c331d533cf98c8394ab91e9c18f7b5f
AliceKB <alicekb01@gmail.com@gmail.com>
2017-05-24 00:56:22 -0400

    Created auth for refreshing login from localsession token.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'auth_user' => 'authentication#authenticate_user'
-   post 'auth_token' => 'authentication#authenticate_token'
+   get 'auth_token' => 'authentication#authenticate_token'
   post 'signup' => 'users#create'
   namespace :v1 do
     resources :days, only: [:show]
