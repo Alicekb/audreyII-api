0154eca1f46b430d2ea7d3c42bf68ec752304e90
AliceKB <alicekb01@gmail.com@gmail.com>
2017-06-14 05:34:16 -0400

    updated state to tracked files to be deleted, deleted files in state and api on save.

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Folendar
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
       end
     end
 
